d38c2d44f25fff3f9b474c42b5e307ec0572cc31
Samuel Conteras <sammycontreras7@gmail.com>
2021-11-18 12:27:25 -0700

    New

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: '741d678abd7e448eb8bc5c5e64ef7d47',
+  accessToken: 'fcc0387246ee451387f9388de523bcd0',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
 
 // record a generic message and send it to Rollbar
-
 rollbar.log('Hello world!')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const sutdents = ["Sam"];
+const students = ["Jeddy"];
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -27,25 +25,25 @@ app.get("/", (req, res) => {
 
 app.get("/api/students", (req, res) => {
     rollbar.info("Someone got the list of students on page load");
-    res.status(200).send(sutdents)
+    res.status(200).send(students);
 });
 
 app.post("/api/students", (req, res) => {
     let {name} = req.body;
     name = name.trim();
 
-    const index = students.findIndex((studentName) => studentName === name)
+    const index = students.findIndex((studentName) => studentName === name);
 
-    if(index === -1 && name != "") {
+    if(index === -1 && name !== "") {
         students.push(name);
-        rollbar.log("Students added successfully", {author: "Sam", type: "manual entry"});
+        rollbar.log("Student added successfully", {author: "Jeddy", type: "manual entry"});
         res.status(200).send(students);
-    } else if (name === "") {
+    } else if(name === "") {
         rollbar.error("No name given");
         res.status(400).send("Must provide a name");
     } else {
         rollbar.error("Student already exists");
-        res.status(400).send("That student already exists")
+        res.status(400).send("That student already exists");
     };
 })
 
@@ -54,5 +52,5 @@ app.use(rollbar.errorHandler());
 const port = process.env.PORT || 4545;
 
 app.listen(port, () => {
-    console.log(`teleportaion to ${port} was successful`)
+    console.log(`They're taking the Hobbits to ${port}`);
 });
